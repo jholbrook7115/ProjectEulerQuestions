96b6729a9472c520362f8a0919641336588b5ca1
Jefferson Holbrook <holbrook002@gmail.com>
2016-04-25 19:08:05 -0400

    Completed Problem 3 with reasonable runtime.

---
+++ ProjectEuler/Problem3.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ProjectEuler
+{
+    public class Problem3
+    {
+
+        public int largestPrimeFactorBrute(long LargeNumber)
+        {
+            List<int> primeNumbers = new List<int>();
+            primeNumbers.Add(2);
+            long number = LargeNumber;
+
+            int highestFactor = 2;
+            if(number == 2)
+            {
+                return highestFactor;
+            }
+            //double sqrtNum = Math.Sqrt(number);
+            double n = number / 2;
+            for(int i = 3; i <= Math.Floor(n); i = i+2)
+            {
+                bool isPrime = false;
+                foreach(int num in primeNumbers)
+                {
+                    if(i%num == 0)
+                    {
+                        //Not a prime number
+                        isPrime = false;
+                        break;
+                    }
+                    else
+                    {
+                        //Is a prime number; add to list of prime numbers
+                        isPrime = true;
+                    }
+                }
+                if (isPrime == true)
+                {
+                    primeNumbers.Add(i);
+                    if (number % i == 0)
+                    {
+                        //it is prime and it is a factor of the number given
+                        highestFactor = i;
+                    }
+                    isPrime = false;
+                }
+                
+            }
+            return highestFactor;
+        }
+        public long largestPrimeFactorSmart(long largeNumber)
+        {
+            long highestFactor = 0;
+            long tmpNumber = largeNumber;
+            int i = 2;
+            while(i * i <= tmpNumber)
+            {
+                if(tmpNumber%i == 0)
+                {
+                    tmpNumber /= i;
+                    highestFactor = i;
+                }
+                else
+                {
+                    i = (i == 2) ? 3 : i + 2;
+                }
+            }
+            if (tmpNumber > highestFactor)
+            {
+                highestFactor = tmpNumber;
+            }
+            return highestFactor;
+        }
+    }
+}
---
+++ ProjectEulerTests/Problem3Tests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectEuler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Tests
+{
+    [TestClass()]
+    public class Problem3Tests
+    {
+        [TestMethod()]
+        public void largestPrimeFactorBruteTest()
+        {
+            Problem3 p = new Problem3();
+            int expected = 7;
+            int actual = p.largestPrimeFactorBrute(14);
+            Assert.AreEqual(expected, actual);
+
+            Problem3 p1 = new Problem3();
+            expected = 5;
+            actual = p1.largestPrimeFactorBrute(100);
+            Assert.AreEqual(expected, actual);
+
+            //The following test will take an (figurative)eternity to run.  
+            //I would not recommend it but try it if you want to i guess.
+           
+            //Problem3 p2 = new Problem3();
+            //expected = 6857;
+            //actual = p.largestPrimeFactorBrute(600851475143);
+            //Assert.AreEqual(expected, actual);
+
+
+        }
+
+        [TestMethod()]
+        public void largestPrimeFactorSmartTest()
+        {
+            Problem3 p2 = new Problem3();
+            long expected = 6857;
+            long actual = p2.largestPrimeFactorSmart(600851475143);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
