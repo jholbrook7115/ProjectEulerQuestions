8653fe695f43f74a22d63d19edd6927bb5711303
Jefferson Holbrook <holbrook002@gmail.com>
2016-04-27 21:02:45 -0400

    Completed a brute force way to solve problem 4

---
+++ ProjectEuler/Problem4.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ProjectEuler
+{
+    public class Problem4
+    {
+        public int largest3DigitPalindromeBrute()
+        {
+            int result = 0;
+            for (int i = 999; i > 900; i--)
+            {
+                for(int j = 999; j > 900; j--)
+                {
+                    int sum = i * j;
+                    if (isPalindrome(sum) == true)
+                    {
+                        Debug.WriteLine(sum);
+                        if (sum > result)
+                        {
+                            result = sum;
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+        public bool isPalindrome(int number)
+        {
+            int n = number;
+            int rev = 0;
+            int tmp = 0;
+            while(number > 0)
+            {
+                tmp = number % 10;
+                rev = rev * 10 + tmp;
+                number /= 10;
+            }
+            if(n == rev)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ ProjectEulerTests/Problem4Tests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectEuler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Tests
+{
+    [TestClass()]
+    public class Problem4Tests
+    {
+        [TestMethod()]
+        public void isPalindromeTest()
+        {
+            Problem4 p = new Problem4();
+            bool expected = true;
+            bool actual = p.isPalindrome(9009);
+            Assert.AreEqual(expected, actual);
+
+            Problem4 p1 = new Problem4();
+            expected = false;
+            actual = p.isPalindrome(1100);
+            Assert.AreEqual(expected, actual);
+
+
+        }
+
+        [TestMethod()]
+        public void largest3DigitPalindromeTest()
+        {
+            Problem4 p = new Problem4();
+            int expected = 906609;
+            int actual = p.largest3DigitPalindromeBrute();
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
